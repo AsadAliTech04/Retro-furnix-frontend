b5efdb14e9aa5e3b9adfb93ab2fe8196b3b65e64
asadalitech <asadalihanif04@gmail.com>
2023-12-03 06:30:06 +0500

    robots.txt updatedt

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+
+const root = ReactDOM.createRoot(document.getElementById("root")); // Use createRoot instead of createRoot
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
